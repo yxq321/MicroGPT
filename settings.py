1f6bedda88540a72639dc30c31134a9cd46ae987
yxq321 <56987208+yxq321@users.noreply.github.com>
2023-08-21 15:55:31 +0800

    add setting

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
 """
 配置文件
 """
+
+# 存放数据信息的文件，只验证过:
+# 1. 英文
+# 2. 文本文件
 data_file = "temp/file.txt"
+
+# 可以从huggingface下载模型文件
+# 例如: https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML (找到里面的 Files and Versions)
 model_file = "../llama.cpp/models/Llama2-Chinese-7b-Chat-ggml-model-q4_0.bin"
